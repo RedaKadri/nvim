faddd2835c96e89e7d02dfeb0ca5e44e29db416e
RedaKadri <redakadri21@gmail.com>
2024-09-22 23:07:02 +0100

    first commit

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,62 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  config = function()
+    local colors = {
+      blue = "#7aa2f7",
+      cyan = "#7dcfff",
+      black = "#15161e",
+      white = "#a9b1d6",
+      red = "#f7768e",
+      violet = "#bb9af7",
+      grey = "#292e42",
+    }
+
+    local bubbles_theme = {
+      normal = {
+        a = { fg = colors.black, bg = colors.cyan },
+        b = { fg = colors.white, bg = colors.grey },
+        c = { fg = colors.white },
+      },
+
+      insert = { a = { fg = colors.black, bg = colors.blue } },
+      visual = { a = { fg = colors.black, bg = colors.violet } },
+      replace = { a = { fg = colors.black, bg = colors.red } },
+
+      inactive = {
+        a = { fg = colors.white, bg = colors.black },
+        b = { fg = colors.white, bg = colors.black },
+        c = { fg = colors.white },
+      },
+    }
+
+    require("lualine").setup({
+      options = {
+        theme = bubbles_theme,
+        component_separators = "",
+        section_separators = { left = "", right = "" },
+      },
+      sections = {
+        lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } },
+        lualine_b = { "filename", "branch" },
+        lualine_c = {
+          "%=", --[[ add your center compoentnts here in place of this comment ]]
+        },
+        lualine_x = {},
+        lualine_y = { "filetype", "progress" },
+        lualine_z = {
+          { "location", separator = { right = "" }, left_padding = 2 },
+        },
+      },
+      inactive_sections = {
+        lualine_a = { "filename" },
+        lualine_b = {},
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = { "location" },
+      },
+      tabline = {},
+      extensions = {},
+    })
+  end,
+}
