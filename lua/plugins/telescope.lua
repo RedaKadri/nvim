883dbfdacb6f6d39fa0a2e8d6aa41924c0d7e457
RedaKadri <redakadri21@gmail.com>
2025-04-28 23:29:58 +0100

    simpler telescope config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,22 +4,21 @@ return {
 		tag = "0.1.8",
 		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
-			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
-			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
-		end,
-	},
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
-		config = function()
+			local actions = require("telescope.actions")
 			require("telescope").setup({
-				extensions = {
-					["ui-select"] = {
-						require("telescope.themes").get_dropdown({}),
+				defaults = {
+					mappings = {
+						i = { -- insert mode
+							["<C-j>"] = actions.preview_scrolling_down,
+							["<C-k>"] = actions.preview_scrolling_up,
+						},
 					},
 				},
 			})
-			require("telescope").load_extension("ui-select")
+
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
 		end,
 	},
 }
